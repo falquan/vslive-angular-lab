4a7f4f8daf06dc45287c7687d1c64cb12b863331
Greg Leppert <greg@automotivemastermind.com>
2017-03-12 16:28:38 -0700

    add shared object comment

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -7,6 +7,10 @@ import { Joke } from './joke.model';
   styleUrls: ['./joke.component.css']
 })
 export class JokeComponent implements OnInit {
+  // This is meant to show that a component would consume
+  // a backing model and then pull out the relevant data
+  // for mutation in the form, rather than mutating the state
+  // of a shared model object.
   joke : Joke = Joke.JOKE
   setup : string;
   punchline : string;
